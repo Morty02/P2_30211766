b97c8e4721d9e0c3afce1b79b3cb689f8799893a
Morty02 <102199827+Morty02@users.noreply.github.com>
2022-04-01 19:21:33 -0400

    Revert "Revert "Update index.js""
    This reverts commit b8de5cfbf504cb0aa7381919ff0b26643c21c24f.

--- aplicacion/routes/index.js
+++ aplicacion/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hola mundo' });
+  res.render('index', { title: 'Curriculum vitae' });
 });
 
 module.exports = router;
