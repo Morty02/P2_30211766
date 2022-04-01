1f7b812e4f68c5b394447a30a8851264fdf4b846
Morty02 <102199827+Morty02@users.noreply.github.com>
2022-04-01 19:26:01 -0400

    Update index.js

--- aplicacion/routes/index.js
+++ aplicacion/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Curriculum vitae' });
+  res.render('index', { title: 'Hola mundo' });
 });
 
 module.exports = router;
