a230520c065efff47cd91198709c3a50e99ee446
Morty02 <102199827+Morty02@users.noreply.github.com>
2022-05-28 22:52:50 -0400

    Task 3

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const data = path.join(__dirname, "data", "db.db");
+const nodemailer = require('nodemailer');
+const { isIPv4 } = require('net');
+const { response } = require('express');
+const { json } = require('express/lib/response');
 const database = new sqlite3.Database(data, err => {
   if(err){
     return console.error(err.message);
@@ -35,56 +39,85 @@ router.get('/contacts', (req, res, next) => {
 });
   
 
-router.post('/', (req, res) => {
-
-  let datetime = new Date();
-  let _date = datetime.toLocaleString();
-  let _time = datetime.toLocaleString();
-  let date = '';
-  let time = '';
-  let ip = req.headers['x-forwarded-for'];
-
-  for(let d = 0; d <= 9; d++){
-      if(_date[d] == '/'){
-        date += '-';
-        continue;
-      }
-      else if(_date[d] == ','){
-        continue;
-      }
-      date += _date[d];
-  }
-
-  for(let t = 11; t <= 23; t++){
-    time += _time[t];
-  }
-
-  if(ip){
-    let ip_ls = ip.split(',');
-    ip = ip_ls[ip_ls.length - 1];
-  }
-  else{
-    console.log('Direccion ip no encontrada');
-  }
-
-  const query = "INSERT INTO contacts(name, email, message, date, time, ip) VALUES (?,?,?,?,?,?);";
-	const messages = [req.body.name, req.body.email, req.body.message, date, time, ip];
-
-	database.run(query, messages, (err)=>{
-	if (err){
-		return console.error(err.message);
-	}
-	else{
-		res.redirect("/");
-    console.log("Un usuario ha comentado");
-	}
-	});
+router.post('/', async (req, res) => {
+        let date = new Date();
+  			let hor = date.getHours();
+  			let min = date.getMinutes();
+  			let sec = date.getSeconds();
+        let form = hor >= 12 ? 'PM' : 'AM'; 
+  			hor = hor % 12; 
+  			hor = hor ? hor : 12; 
+  			min = min < 10 ? '0' + min : min;
+        //var response = grecaptcha.getResponse();
+        let time = hor + ':' + min + ':' + sec + ' ' + form;
+        let T_Date = date.getDate() + '-' + ( date.getMonth() + 1 ) + '-' + date.getFullYear();
+        /*const response_key = req.body["g-recaptcha-response"];
+        const secret_key = "6Ld31CMgAAAAADt2dw7uV14D8x0pc9t4kxpKZvaZ";
+        const resp = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
+          let atrib = json_decode(resp, true);
+          if(!atrib["success"]){*/ //No conseguí que funcionara :c
+           
           
-});
+
+          var ip = req.headers['x-forwarded-for'];
+            if(ip){
+              let ip_ls = ip.split(',');
+              ip = ip_ls[ip_ls.length - 1];
+            }
+            else{
+              console.log('Direccion ip no encontrada');
+            }
+            const { name, email, message} = req.body;
+            contentHTML = `
+            <h1>Datos del usuario</h1>
+            <ul>
+                <li>Nombre: ${name}</li>
+                <li>Email: ${email}</li>
+                <li>Ip:  ${ip} </li>
+                <li>Fecha: ${T_Date} </li>
+                <li>Hora: ${time} </li>
+
+            </ul>
+            <p>${message}</p>
+          `;
+          let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'cp06685@gmail.com',
+              pass: 'zgdh kqvy qkcg rmvt'
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+          });
+          let info = await transporter.sendMail({
+            from: '"Task 3 P2" <cp06685@gmail.com>',
+            to: 'programacion2ais@dispostable.com',
+            subject: 'Formulario de contatos',
+            html: contentHTML
+          })
+
+            const query = "INSERT INTO contacts(name, email, message, date, time, ip) VALUES (?,?,?,?,?,?);";
+            const messages = [req.body.name, req.body.email, req.body.message, T_Date, time, ip];
+
+            database.run(query, messages, (err)=>{
+            if (err){
+              return console.error(err.message);
+            }
+            else{
+              res.redirect("/");
+            }
+            });  
+          /* }else{
+         console.log("Por fa valida el capcha");
+       };*/
+    })
 
 
 router.get('/', (req, res, next) => {
   res.render('index.ejs',{data:{}});
 });
+
+
 
 module.exports = router;
